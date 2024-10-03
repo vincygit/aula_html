c46fce8f169345a8674e0faee17e305ab98657ec
vincygit <vincygit@gmail.com>
2024-10-03 15:43:32 -0300

    Flask 03/10

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
-from flask import Flask # importa o flask
+from flask import (Flask, request) # importa o flask
 
 app = Flask(__name__) # cria uma instância
 
-@app.route("/", methods=('GET' ,)) # Assina uma rota
-def index(): # Função rsponsável pela página
-    return "<h1>Página inicial</h1>" # HTML retornado
+@app.route("/<string:nome>", methods=('GET' ,)) # Assina uma rota
+def index(nome): # Função rsponsável pela página
+    # HTML retornado
+    return f"""<h1>Página Inicial</h1>
+        <p>Olá {nome}, que nome bonito!</p>
+    """
 
-@app.route("/outra_pagina", methods=('GET' ,))
-def outra():
-    return "<h1>Outra página</h1>"
+@app.route("/galeria", methods=('GET' ,))
+def galeria():
+    return "<h1>Galeria</h1>"
+
+@app.route("/contato", methods=('GET' ,))
+def contato():
+    return "<h1>Contato</h1>"
+
+@app.route("/sobre", methods=('GET' ,))
+def sobre():
+    return "<h1>Sobre Nós</h1>"
+
+@app.route("/personagens", methods=('GET' ,))
+def personagens():
+    per1 = request.args.get('per1')
+    per2 = request.args.get('per2')
+    per3 = request.args.get('per3')
+
+    return f"""<h1>Página Inicial</h1>
+        <ul><li>{per1}</li><li>{per2}</li><li>{per3}</li></ul>
+    """
+
+
+@app.route("/potencial/<string:n1>/<string:n2>", methods=('GET' ,))
+def potencial(n1, n2):
+    r = float(n1) ** float(n2)
+    return f""" <h1>{n1} ^ {n2} = {r} </h1>"""
