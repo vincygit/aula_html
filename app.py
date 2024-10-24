32ad6e22c5035cedb988f2a8d7077023aaab8f78
vincygit <vincygit@gmail.com>
2024-10-24 15:31:27 -0300

    Flask - Layout 24/10

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import (Flask, render_template, request) # importa o flask
+from flask import (Flask, flash, redirect, render_template, request, url_for) # importa o flask
 
 app = Flask(__name__) # cria uma instância
+app.secret_key = 'segredo'  # Para utilizar flash messages
 
 @app.route("/<string:nome>", methods=('GET' ,)) # Assina uma rota
 def index(nome): # Função rsponsável pela página
@@ -51,6 +52,52 @@ def potencial(n1, n2):
     r = float(n1) ** float(n2)
     return render_template('potencial.html', n1=n1, n2=n2, resultado=r)
 
-@app.route("/tabuada/<int:numero>", methods=('GET' ,))
-def tabuada(numero):
-    return render_template('tabuada.html', numero=numero)
+
+@app.route("/tabuada")
+@app.route("/tabuada/<numero>", methods=("GET" , ))
+def tabuada(numero = None): # Desobriga o valor
+
+    if 'numero' in request.args: # se arguento existir
+        numero = int(request.args.get('numero')) # atualiza numero
+
+    return render_template('tabuada.html', numero=numero)  
+
+
+@app.route('/calculojuros', methods=['GET', 'POST'])
+def calculo_juros():
+    if request.method == 'POST':
+        try:
+            investimento_inicial = float(request.form['investimento_inicial'])
+            juros_ano = float(request.form['juros_ano'])
+            tempo_meses = int(request.form['tempo_meses'])
+            aporte_mensal = float(request.form['aporte_mensal'])
+
+            # Cálculo de juros
+            juros_mensal = juros_ano / 12 / 100
+            total = investimento_inicial
+
+            for _ in range(tempo_meses):
+                total += aporte_mensal
+                total *= (1 + juros_mensal)
+
+            return render_template('calculo_juros.html', total=total)
+        except ValueError:
+            return render_template('calculo_juros.html', error="Por favor, insira valores válidos.")
+
+    return render_template('calculo_juros.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        senha = request.form['senha']
+        
+        # Verificação de credenciais
+        if email == 'aluno@senai.br' and senha == 'senai':
+            flash('Usuário Logado com Sucesso!', 'success')
+            return redirect(url_for('login'))
+        else:
+            flash('Usuário ou senha incorretos, tente novamente.', 'error')
+
+    return render_template('login.html')
